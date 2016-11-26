b837a429ab0f3dd4d201ea715e9aaed1d4ce2c4b
SAPHETY\djsantos <david.santos@saphety.com>
2016-11-26 18:25:12 +0000

    Added Goalkeeper "AI"

--- Assets/Scripts/Goalkeeper.cs
+++ Assets/Scripts/Goalkeeper.cs
@@ -6,23 +6,32 @@ public class Goalkeeper : MonoBehaviour {
 
 	public float width = 10f;
 	public float height = 5f;
+	public float delayReflexes = 0.5f;
 	private bool movingRight = true;
-	
+
 	public float speed = 15.0f;
-	
+
 	float xmin = -3f;
 	float xmax = 3;
-	
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		//Make Goalkeeper look at the Player all the time
 		transform.LookAt(Camera.main.transform.position, Vector3.up);
 
+		if (ballPrefab.GetComponent<ShootMe> ().IsFlying) {
+			StartCoroutine (JumpToBall(delayReflexes));
+		} else {
+			IdleMovement ();
+		}
+
+	}
+	void IdleMovement () {
 		//Handle Goalkeeper movement
 		if (movingRight)
 		{
@@ -31,7 +40,7 @@ public class Goalkeeper : MonoBehaviour {
 		else {
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
-		
+
 		float rightEdgeOfFormation = transform.position.x + (0.5f * width);
 		float leftEdgeOfFormation = transform.position.x - (0.5f * width);
 		if (leftEdgeOfFormation < xmin)
@@ -41,6 +50,16 @@ public class Goalkeeper : MonoBehaviour {
 		else if (rightEdgeOfFormation > xmax)
 		{
 			movingRight = false;
+		}
+	}
+	IEnumerator JumpToBall(float waitForSeconds) {
+		yield return new WaitForSeconds (waitForSeconds);
+		if (transform.position.x - ballPrefab.transform.position.x < 0 ) {
+			if (xmax > transform.position.x)
+				transform.position += Vector3.right * speed * 2 * Time.deltaTime;
+		} else {
+			if (xmin < transform.position.x)
+				transform.position += Vector3.left * speed * 2 *Time.deltaTime;
 		}
 	}
 }
