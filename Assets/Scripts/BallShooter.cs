7fb1b34a2b7866146be25caa36721467078b6d90
João Eiras Antunes <jantunes91@gmail.com>
2016-11-26 04:31:39 +0000

    Added: -Persistent Crowd Sounds -Basic ball shooting handling Fixed/Changed: -Stadium had no collision -Changed goalkeeper sprite for the right one

---
+++ Assets/Scripts/BallShooter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallShooter : MonoBehaviour {
+	private Vector3 shooterOffset;
+
+	public GameObject ballPrefab;
+	// Use this for initialization
+	void Start () {
+		shooterOffset = new Vector3(0.0f, -0.4f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GvrViewer.Instance.VRModeEnabled && GvrViewer.Instance.Triggered){
+			Debug.Log("Tap detected");
+			Vector3 shooterOrigin = Camera.main.transform.position + shooterOffset;
+			float playerDirectionX = Camera.main.transform.rotation.x;
+			float playerDirectionY = Camera.main.transform.rotation.y;
+			float playerDirectionYinAngles = Camera.main.transform.rotation.eulerAngles.y;
+
+			GameObject ball = Instantiate(ballPrefab, shooterOrigin, Quaternion.identity) as GameObject;
+			ball.transform.RotateAround(Camera.main.transform.position, Vector3.up, playerDirectionYinAngles);
+			ball.GetComponent<Rigidbody>().velocity = new Vector3(playerDirectionY*10, playerDirectionX*5, 10);
+		}
+
+	}
+}
